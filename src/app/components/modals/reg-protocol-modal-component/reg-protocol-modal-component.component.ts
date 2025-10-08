5d7f74e60dd0f1abc4bbd353f94b460e5a8bfa37
Anton Konstantynov <batann1990@gmail.com>
2025-10-08 23:15:53 +0300

    Доробити додавання протоколу в базу

--- src/app/components/modals/reg-protocol-modal-component/reg-protocol-modal-component.component.ts
+++ src/app/components/modals/reg-protocol-modal-component/reg-protocol-modal-component.component.ts
@@ -20,7 +20,8 @@ export class RegProtocolModalComponentComponent {
   measurementsArray = model.required<string[]>()
   currentInstruction = model.required<Instruction>()
 
-  private isEmptyResultProtocol: boolean = true
+  private isEmptyResultProtocol: boolean = true;
+  private currentShownProtocolIndex: number = 0;
 
   @ViewChildren('equipmentInfoTextInput') equipmentInfoTextInput!: QueryList<ElementRef<HTMLInputElement>>;
   @ViewChildren('equipmentNoteTextInput') equipmentNoteTextInput!: QueryList<ElementRef<HTMLInputElement>>;
@@ -38,7 +39,6 @@ export class RegProtocolModalComponentComponent {
   @ViewChildren('measurementTextInputNumber') measurementTextInputNumber!: QueryList<ElementRef<HTMLInputElement>>;
   @ViewChildren('measurementTextInputDate') measurementTextInputDate!: QueryList<ElementRef<HTMLInputElement>>;
   @ViewChildren('measurementCheckboxInput') measurementCheckboxInput!: QueryList<ElementRef<HTMLInputElement>>;
-
 
 
   equipmentInfoArray = signal<string[]>([])
@@ -77,8 +77,7 @@ export class RegProtocolModalComponentComponent {
       this.jobResultCheckboxInput?.forEach(i => i.nativeElement.checked = false)
       this.extraInfoCheckboxInput?.forEach(i => i.nativeElement.checked = false)
       this.measurementCheckboxInput?.forEach(i => i.nativeElement.checked = false)
-
-
+      this.currentShownProtocolIndex = 0
     });
   }
 
@@ -94,21 +93,19 @@ export class RegProtocolModalComponentComponent {
       this.extraInfoArray.update((arr) => [...arr, input.nativeElement.value]);
     })
 
-    this.equipmentInfoObject.set(this.currentProtocol().equipmentInfo!
-      .map((val, index) => {
-        return {
-          [val]: this.equipmentInfoArray()[index],
-          note: [...this.equipmentNoteTextInput][index].nativeElement.value,
-        }
-      }))
+    this.equipmentInfoObject.set(this.currentProtocol().equipmentInfo!.map((val, index) => {
+      return {
+        [val]: this.equipmentInfoArray()[index],
+        note: [...this.equipmentNoteTextInput][index].nativeElement.value,
+      }
+    }))
 
-    this.extraInfoObject.set(this.currentProtocol().extraInfo!
-      .map((val, index) => {
-        return {
-          [val]: this.extraInfoArray()[index],
-          note: [...this.extraInfoNoteTextInput][index].nativeElement.value,
-        }
-      }))
+    this.extraInfoObject.set(this.currentProtocol().extraInfo!.map((val, index) => {
+      return {
+        [val]: this.extraInfoArray()[index],
+        note: [...this.extraInfoNoteTextInput][index].nativeElement.value,
+      }
+    }))
 
     const jobRowsArray: string[][] = [];
     const jobNotesArray: string[][] = [];
@@ -133,22 +130,21 @@ export class RegProtocolModalComponentComponent {
       jobNotesArray.push(jobNotes)
     }
 
-    this.jobsResultsArray.set(this.currentProtocol().jobs
-      .map((val, index) => {
-        const currentRow = jobRowsArray[index];
-        const currentNote = jobNotesArray[index];
-        const resArray = val.jobsDesc
-          .map((desc, i) => {
-            return {
-              [desc as string]: currentRow[i],
-              note: currentNote[i]
-            }
-          })
-        return {
-          number: val.number,
-          jobsDesc: resArray
-        }
-      }))
+    this.jobsResultsArray.set(this.currentProtocol().jobs.map((val, index) => {
+      const currentRow = jobRowsArray[index];
+      const currentNote = jobNotesArray[index];
+      const resArray = val.jobsDesc
+        .map((desc, i) => {
+          return {
+            [desc as string]: currentRow[i],
+            note: currentNote[i]
+          }
+        })
+      return {
+        number: val.number,
+        jobsDesc: resArray
+      }
+    }))
 
     const measurementsTitles: string[] = this.measurementTextInputTitle.map((t) => t.nativeElement.value)
     const measurementsTypes: string[] = this.measurementTextInputType.map((t) => t.nativeElement.value)
@@ -164,66 +160,187 @@ export class RegProtocolModalComponentComponent {
       }
     }))
 
-    if (this.isEmptyResultProtocol) {
-      this.resultProtocol.set([{
+    this.resultProtocol.update(registeredProtocol => {
+      const copy = [...registeredProtocol];
+      copy[this.currentShownProtocolIndex] = {
         equipmentInfo: this.equipmentInfoObject(),
-        jobs: this.jobsResultsArray(),
         extraInfo: this.extraInfoObject(),
-        measurements: this.measurements()
-      }])
-      this.isEmptyResultProtocol = false
-    } else {
-      this.resultProtocol.set([...this.resultProtocol(), {
-        equipmentInfo: this.equipmentInfoObject(),
         jobs: this.jobsResultsArray(),
-        extraInfo: this.extraInfoObject(),
         measurements: this.measurements()
-      }])
-    }
+      }
+      return copy
+    })
+
+    this.currentShownProtocolIndex = this.resultProtocol().length;
 
     console.log(this.resultProtocol())
 
     this.equipmentInfoCheckboxInput.forEach((checkbox, index) => {
-        if (!checkbox.nativeElement.checked) {
-          [...this.equipmentInfoTextInput][index].nativeElement.value = "";
-          [...this.equipmentNoteTextInput][index].nativeElement.value = "";
-        }
+      if (!checkbox.nativeElement.checked) {
+        [...this.equipmentInfoTextInput][index].nativeElement.value = "";
+        [...this.equipmentNoteTextInput][index].nativeElement.value = "";
       }
-    )
+    })
 
-    this.jobResultCheckboxInput.forEach((checkbox) => {
-        if (!checkbox.nativeElement.checked) {
-          (checkbox.nativeElement.parentElement!.previousElementSibling?.previousElementSibling?.firstElementChild as HTMLInputElement).value = "";
-          (checkbox.nativeElement.parentElement!.previousElementSibling?.firstElementChild as HTMLInputElement).value = ""
-        }
+    this.jobResultCheckboxInput.forEach(checkbox => {
+      if (!checkbox.nativeElement.checked) {
+        (checkbox.nativeElement.parentElement!.previousElementSibling?.previousElementSibling?.firstElementChild as HTMLInputElement).value = "";
+        (checkbox.nativeElement.parentElement!.previousElementSibling?.firstElementChild as HTMLInputElement).value = ""
       }
-    )
+    })
 
     this.extraInfoCheckboxInput.forEach((checkbox, index) => {
-        if (!checkbox.nativeElement.checked) {
-          [...this.extraInfoTextInput][index].nativeElement.value = "";
-          [...this.extraInfoNoteTextInput][index].nativeElement.value = "";
-        }
+      if (!checkbox.nativeElement.checked) {
+        [...this.extraInfoTextInput][index].nativeElement.value = "";
+        [...this.extraInfoNoteTextInput][index].nativeElement.value = "";
       }
-    )
+    })
 
     this.measurementCheckboxInput.forEach((checkbox, index) => {
-        if (!checkbox.nativeElement.checked) {
-          [...this.measurementTextInputTitle][index].nativeElement.value = "";
-          [...this.measurementTextInputType][index].nativeElement.value = "";
-          [...this.measurementTextInputNumber][index].nativeElement.value = "";
-          [...this.measurementTextInputDate][index].nativeElement.value = "";
-        }
+      if (!checkbox.nativeElement.checked) {
+        [...this.measurementTextInputTitle][index].nativeElement.value = "";
+        [...this.measurementTextInputType][index].nativeElement.value = "";
+        [...this.measurementTextInputNumber][index].nativeElement.value = "";
+        [...this.measurementTextInputDate][index].nativeElement.value = "";
       }
-    )
-
+    })
   }
 
-  showPrevEquipment() {
-    console.log("showPrevEquipment clicked")
+  showProtocolInfoByProtocolIndex() {
+    this.equipmentInfoTextInput.forEach((input, i) => {
+      const infos: string[] = Object.values([...this.resultProtocol()][this.currentShownProtocolIndex].equipmentInfo[i]);
+      input.nativeElement.value = infos[0];
+      [...this.equipmentNoteTextInput][i].nativeElement.value = infos[1];
+    })
+
+    this.extraInfoTextInput.forEach((input, i) => {
+      const extraInfos: string[] = Object.values([...this.resultProtocol()][this.currentShownProtocolIndex].extraInfo[i]);
+      input.nativeElement.value = extraInfos[0];
+      [...this.extraInfoNoteTextInput][i].nativeElement.value = extraInfos[1];
+    })
+
+    this.measurementTextInputTitle.forEach((input, i) => {
+      const measurementInfo: string[] = Object.values([...this.resultProtocol()][this.currentShownProtocolIndex].measurements[i]);
+      input.nativeElement.value = measurementInfo[0];
+      [...this.measurementTextInputType][i].nativeElement.value = measurementInfo[1];
+      [...this.measurementTextInputNumber][i].nativeElement.value = measurementInfo[2];
+      [...this.measurementTextInputDate][i].nativeElement.value = measurementInfo[3];
+    });
+
+    const jobsArray: any = [];
+    this.resultProtocol()[this.currentShownProtocolIndex].jobs
+      .forEach(job => (Object.values(job)[1] as {}[])
+        .forEach(jobIndex => jobsArray.push(Object.values(jobIndex)))
+      )
+
+    this.jobRow.forEach((j, index) => {
+      const jobResInput = j.nativeElement.nextElementSibling?.children[0] as HTMLInputElement;
+      jobResInput.value = jobsArray[index][0]
+      const jobNoteInput = j.nativeElement.nextElementSibling?.nextElementSibling?.children[0] as HTMLInputElement;
+      jobNoteInput.value = jobsArray[index][1]
+    })
+  }
+
+  showPrevEquipmentProtocol() {
+    if (this.currentShownProtocolIndex < 1) return
+    this.currentShownProtocolIndex -= 1;
+    this.showProtocolInfoByProtocolIndex()
+  }
+
+  showNextEquipmentProtocol() {
+    if (this.currentShownProtocolIndex >= this.resultProtocol().length - 1) return
+    this.currentShownProtocolIndex += 1;
+    this.showProtocolInfoByProtocolIndex()
   }
 
   registerProtocol() {
     console.log("registerProtocol clicked")
+  }
+
+  /*-------Калькулятор значень в комірках протоколу------------*/
+  @ViewChildren('jobResultTextInput') resultInputs!: QueryList<ElementRef<HTMLInputElement>>;
+  private cellMap = new Map<string, HTMLInputElement>();
+  private activeFormulaInput: HTMLInputElement | null = null;
+
+  private ensureCellMap() {
+    this.cellMap.clear();
+    this.resultInputs.forEach(el => {
+      const input = el.nativeElement;
+      const name = input.dataset['cell'];
+      if (name) this.cellMap.set(name, input);
+    });
+  }
+
+  onFocus(input: HTMLInputElement) {
+    this.ensureCellMap();
+
+    const formula = input.dataset['formula'];
+    if (formula !== undefined) {
+      input.value = formula;
+    }
+
+    this.activeFormulaInput = input;
+  }
+
+  onInput(input: HTMLInputElement) {
+    if (input.value.startsWith('=')) {
+      input.dataset['formula'] = input.value;
+    } else {
+      delete input.dataset['formula'];
+    }
+  }
+
+  onBlur() {
+    this.activeFormulaInput = null;
+    setTimeout(() => this.recalcAll(), 0);
+  }
+
+  onMouseDown(e: MouseEvent, input: HTMLInputElement) {
+    if (this.activeFormulaInput &&
+      this.activeFormulaInput !== input &&
+      this.activeFormulaInput.dataset['formula'] &&
+      document.activeElement === this.activeFormulaInput) {
+      e.preventDefault();
+      this.activeFormulaInput.value += input.dataset['cell'];
+      this.activeFormulaInput.dataset['formula'] = this.activeFormulaInput.value;
+    }
+  }
+
+  private recalcAll() {
+    this.ensureCellMap();
+    this.resultInputs.forEach(el => {
+      const input = el.nativeElement;
+      if (input.dataset['formula'] && input !== document.activeElement) {
+        const val = this.computeCell(input.dataset['cell']!);
+        input.value = isNaN(val) ? 'ERR' : String(val);
+      }
+    });
+  }
+
+  private computeCell(name: string, seen: Set<string> = new Set()): number {
+
+    const el = this.cellMap.get(name);
+    if (!el) return 0;
+    if (seen.has(name)) return NaN;
+    seen.add(name);
+
+    const formula = el.dataset['formula'];
+    if (formula && formula.startsWith('=')) {
+      let body = formula.slice(1);
+      body = body.replace(/[A-Z]+[0-9]+|R[0-9]+/g, ref => {
+        const v = this.computeCell(ref, new Set(seen));
+        return !isNaN(v) ? String(v) : 'NaN';
+      });
+      if (!/^[0-9+\-*/().\sNaN]+$/.test(body)) return NaN;
+      try {
+        const result = Function('"use strict"; return (' + body + ')')();
+        return (typeof result === 'number' && isFinite(result)) ? result : NaN;
+      } catch {
+        return NaN;
+      }
+    } else {
+      const v = parseFloat(el.value);
+      return isNaN(v) ? 0 : v;
+    }
   }
 }
